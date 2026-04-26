661025b53843fb453de97c2081df13ee0184fc13
Roberto CR <249939344+gitrcr@users.noreply.github.com>
2026-04-26 20:02:02 +0200

    Update new-az-sp.sh

--- bin/new-az-sp.sh
+++ bin/new-az-sp.sh
@@ -2,7 +2,7 @@
 # 0. Ejecución directa: copia el contenido del comentario DIRECT para ejecutar el script, el fichero se borra al finalizar (comentar para revisión).
 
 <<DIRECT
-wget https://raw.githubusercontent.com/gitrcr/azure/refs/heads/main/bin/sp_account.sh
+wget https://raw.githubusercontent.com/gitrcr/azure/refs/heads/main/bin/new-az-sp.sh
 chmod +x new-az-sp.sh
 ./new-az-sp.sh
 DIRECT
